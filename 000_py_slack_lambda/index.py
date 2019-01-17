c985f0f28e6a9d5492a27adb90ca13ff3d227f3f
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-17 05:42:39 -0800

    Update index.py

--- 000_py_slack_lambda/index.py
+++ 000_py_slack_lambda/index.py
@@ -1,12 +1,17 @@
 import json
 from botocore.vendored import requests
 
+print('Loading function')
+
 def lambda_handler(event, context):
     
     URL = '[YOUR_SLACK_INCOMING_WEBHOOK_URL]'
     message = 'Hello from AWS Lambda'
     data = '{{"text":"{}"}}'.format(message)
     response = requests.post(URL, data=data, headers={'Content-Type': 'application/json'})
+    
+    print('Response: ' + str(response.text))
+    print('Response code: ' + str(response.status_code))
     
     return {
         'statusCode': response.status_code,
